c67f421680bb7ba3013da7d3e42e4eb3c19f6f95
mushonnip <mushonnipabu@gmail.com>
2020-03-30 15:08:19 +0700

    install admin

--- uyifutsal/settings.py
+++ uyifutsal/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # General use templates & template tags (should appear first)
+    'adminlte3',
+     # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
