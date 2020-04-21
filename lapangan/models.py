d96cdd43dedad23988d622070380ac2f5c1ffed0
mushonnip <mushonnipabu@gmail.com>
2020-04-22 00:39:25 +0700

    add complete db seeds

--- lapangan/models.py
+++ lapangan/models.py
@@ -8,7 +8,6 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     point = models.IntegerField(default=0)
-    birth_date = models.DateField(null=True, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
